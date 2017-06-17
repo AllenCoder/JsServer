a2d2216ebbfabc26a2806397b24cea3e2af01a19
AllenCoder <AllenCoder@126.com>
2017-06-17 18:25:26 +0800

    update data

--- app.js
+++ app.js
@@ -17,8 +17,11 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.text({ type: 'text/html' }));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.bodyParser())
+// app.use(bodyParser.json())
+app.use(bodyParser.text())
+app.use(bodyParser.raw());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,57 @@ router.all('/title', function(req, res, next) {
   console.log(req)
   // res.render('index', { title: 'Express' });
 })
+router.get('/api/users', function(req, res) {
+    var user_id = req.param('id');
+    var token = req.param('token');
+    var geo = req.param('geo');
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});
+router.param('name', function(req, res, next, name) {
+
+    // check if the user with that name exists
+    // do some validations
+    // add -dude to the name
+    var modified = name + '-dude';
+
+    // save name to the request
+    req.name = modified;
+
+    next();
+});
+
+// http://localhost:8080/api/users/chris
+router.get('/api/users/:name', function(req, res) {
+    // the user was found and is available in req.user
+    res.send('What is up ' + req.name + '!');
+});
 router.post('/test', function(req, res, next) {
 
     console.log(req)
     // res.sendfile('public/res/jcodds.xml');
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+
+    console.error(user_id)
+    console.error(token)
+
+    console.error(geo)
+
+
     res.sendFile('jcodds.xml', { root: path.join(__dirname, '../public/res') });
     // res.render('index', { title: 'Express' });
 })
+
+router.post('/api/users', function(req, res) {
+    console.log(req)
+    var user_id = req.body.gid;
+    var token = req.body.osversion;
+    console.log(user_id)
+    console.log(token)
+    res.sendFile('jcodds.xml', { root: path.join(__dirname, '../public/res') });
+
+});
 
 module.exports = router;
